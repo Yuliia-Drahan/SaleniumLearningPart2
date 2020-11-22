ebe04be8bb9eee2d943397f8c351e6f0c611143f
Yuliia-Drahan <dragan1925@gmail.com>
2020-11-22 14:37:41 +0200

    Checkboxes

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,6 +1,11 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class MainClass {
@@ -10,8 +15,17 @@ public class MainClass {
     public static void main(String[] args){
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\IdeaProjects\\TestProject\\drivers\\chromedriver.exe");
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.foxtrot.com.ua/");
 
-        driver.get("https://github.com/");
+
+        WebElement checkBox = driver.findElement(By.xpath("//ul[@class='navigator-ul-center']/li/a[@href='/collection/bags' and contains(text(),'Сумки')]"));
+
+        CheckBox bagCheckBox = new CheckBox(checkBox, driver);
+        bagCheckBox.checkElement(checkBox);
+        bagCheckBox.isChecked(checkBox);
+        bagCheckBox.SelectRadioButton("c-2334343", checkBox, driver);
+
 
 
 
