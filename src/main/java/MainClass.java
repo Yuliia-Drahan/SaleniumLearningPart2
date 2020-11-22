7fca3a35515bcd3aab714c6d5076997b4684a7f1
Yuliia-Drahan <dragan1925@gmail.com>
2020-11-22 15:14:57 +0200

    Checkboxes foxtrot

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -2,8 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,16 +16,17 @@ public class MainClass {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.foxtrot.com.ua/");
+        Actions actions = new Actions(driver);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//li[@data-category='32014']/span[@class='icon dropdown-arrow']")).click();
+        driver.findElement(By.xpath("//a[@href=\"/ru/shop/mobilnye_telefony_smartfon.html\"]")).click();
 
-
-        WebElement checkBox = driver.findElement(By.xpath("//ul[@class='navigator-ul-center']/li/a[@href='/collection/bags' and contains(text(),'Сумки')]"));
+        WebElement checkBox = driver.findElement(By.xpath("//span[text()=\"SAMSUNG\"]"));
 
         CheckBox bagCheckBox = new CheckBox(checkBox, driver);
         bagCheckBox.checkElement(checkBox);
         bagCheckBox.isChecked(checkBox);
-        bagCheckBox.SelectRadioButton("c-2334343", checkBox, driver);
-
-
+        bagCheckBox.SelectRadioButton("XIAOMI", checkBox, driver);
 
 
     }
