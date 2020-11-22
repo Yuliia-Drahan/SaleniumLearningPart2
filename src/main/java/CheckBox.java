bac491701578b13abb6c8a7c5940c7a425b89705
Yuliia-Drahan <dragan1925@gmail.com>
2020-11-22 15:21:49 +0200

    Class

---
+++ src/main/java/CheckBox.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CheckBox {
+
+    private WebElement checkBox;
+    private WebDriver driver;
+
+    //Конструктор класу
+    public CheckBox (WebElement checkBox, WebDriver driver){
+        this.checkBox = checkBox;
+        this.driver = driver;
+    }
+
+    //Вибираємо чек-бокс
+    public void checkElement(WebElement button){
+        button.click();
+    }
+
+    //Перевіряємо чи елемент селектнутий
+    public void isChecked (WebElement button){
+        if (button.isSelected()){
+            System.out.println("This Element is selected");
+        } else
+            System.out.println("This element is not selected!");
+    }
+
+
+    //Якщо елемент не селектнутий, то вибираємо його
+    public static void SelectRadioButton (String name, WebElement button, WebDriver driver){
+        String buttonXpath = "//span[text()=\"%s\"]";
+        if (!button.isSelected()){
+            driver.findElement(By.xpath(String.format(buttonXpath,name))).click();
+        }
+    }
+
+}
